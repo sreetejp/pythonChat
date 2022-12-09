ece750d135b5c19ce0d4e3bc8b50f817997210d5
Sreetej Ponnam <90596174+sreetejp@users.noreply.github.com>
2022-12-09 11:50:43 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,18 +1,8 @@
 import socket
 from threading import Thread
 
-SERVER_HOST = "0.0.0.0"
-SERVER_PORT = 5184
-separator_token = "<SEP>"
 
-client_sockets = set()
-s = socket.socket()
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((SERVER_HOST, SERVER_PORT))
-s.listen(5)
-print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
-
-def listen_for_client(cs):
+def listen_for_client(cs, separator):
     while True:
         try:
             msg = cs.recv(1024).decode()
@@ -20,19 +10,31 @@ def listen_for_client(cs):
             print(f"[!] Error: {e}")
             client_sockets.remove(cs)
         else:
-            msg = msg.replace(separator_token, ": ")
+            msg = msg.replace(separator, ": ")
         for client_socket in client_sockets:
             client_socket.send(msg.encode())
 
-while True:
-    client_socket, client_address = s.accept()
-    print(f"[+] {client_address} connected.")
-    client_sockets.add(client_socket)
-    t = Thread(target=listen_for_client, args=(client_socket,))
-    t.daemon = True
-    t.start()
-    
-for cs in client_sockets:
-    cs.close()
 
-s.close()
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 5184
+separator_token = "<SEP>"
+
+client_sockets = set()
+
+with socket.socket() as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((SERVER_HOST, SERVER_PORT))
+    s.listen(5)
+    print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+
+    while True:
+        try:
+            client_socket, client_address = s.accept()
+        except Exception as e:
+            print(f"[!] Error: {e}")
+        else:
+            print(f"[+] {client_address} connected.")
+            client_sockets.add(client_socket)
+            t = Thread(target=listen_for_client, args=(client_socket, separator_token))
+            t.daemon = True
+            t.start()
